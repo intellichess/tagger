b0b4dbdee22c545c8190831482e23f5f0d8ff368
wdaws <bill.d.daws@gmail.com>
2018-03-28 01:53:58 -0400

    messing with printing board state

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import chess
 
+
 def main():
     print("hello")
     b = chess.Board()
     print(b.legal_moves)
+    b.push_san("Nh3")
+    print(b.legal_moves)
+    print(b)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
